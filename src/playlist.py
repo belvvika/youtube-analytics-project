0624822e084e628b034f5282c06634c18a2ccac0
belvvika <134885414+belvvika@users.noreply.github.com>
2023-11-02 13:45:33 +0300

    update playlist.py

--- src/playlist.py
+++ src/playlist.py
@@ -1,6 +1,7 @@
 from googleapiclient.discovery import build
 import os
 from dotenv import load_dotenv
+import datetime
 
 class PlayList():
     load_dotenv()
@@ -10,7 +11,7 @@ class PlayList():
         playlists = self.get_service().playlists().list(channelId=channel_id, part='contentDetails,snippet', maxResults=50).execute()
         self.title = playlists['items'][0]
         self.url = f'https://www.youtube.com/watch?v=WNpfcqhNGhM&list={playlist}'
-        self.duration = playlists['items']['contentDetails']['duration']
+
         video_statistic = self.get_service().videos().list(part='snippet,statistics,contentDetails,topicDetails', id=video_id).execute()
         self.like_count = video_statistic['items'][0]['statistics']['likeCount']
 
@@ -22,7 +23,10 @@ class PlayList():
 
     @property
     def total_duration(self):
-        self.duration
+        for video in video_statistic['items']:
+            iso_8601_duration = video['contentDetails']['duration']
+            duration = isodate.parse_duration(iso_8601_duration)
+            return duration
 
     def show_best_video(self):
         pass
