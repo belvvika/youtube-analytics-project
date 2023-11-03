5e5f41322ab39e6fdfa331fc44277cb793ea6bb1
belvvika <134885414+belvvika@users.noreply.github.com>
2023-11-03 17:15:24 +0300

    update playlist.py

--- src/playlist.py
+++ src/playlist.py
@@ -1,20 +1,28 @@
 from googleapiclient.discovery import build
 import os
 from dotenv import load_dotenv
+from src.channel import Channel
+from src.video import Video
 import datetime
 
-class PlayList():
+class PlayList(Channel, Video):
     load_dotenv()
     api_key = os.getenv('YOUTUBE_API')
-    def __init__(self, playlist) -> None:
-        self.playlist = playlist
-        playlists = self.get_service().playlists().list(channelId=channel_id, part='contentDetails,snippet', maxResults=50).execute()
-        self.title = playlists['items'][0]
-        self.url = f'https://www.youtube.com/watch?v=WNpfcqhNGhM&list={playlist}'
+    def __init__(self, video_id, channel_id, playlist_id) -> None:
+        self.playlist_id = playlist_id
+
+        super().__init__(video_id, channel_id)
+
+        playlist_videos = self.get_service().playlistItems().list(playlistId=playlist_id, part='contentDetails', maxResults=50).execute()
+        '''получение данных по плейлистам канала'''
+
+        self.title = playlist_videos['items'][0]
+        self.url = f'https://www.youtube.com/watch?v=WNpfcqhNGhM&list={playlist_videos}'
 
         video_statistic = self.get_service().videos().list(part='snippet,statistics,contentDetails,topicDetails', id=video_id).execute()
         self.like_count = video_statistic['items'][0]['statistics']['likeCount']
 
+        self.video_list = [video['contentDetails']['videoId'] for video in playlist_videos['items']]
     @classmethod
     def get_service(cls):
         youtube_api_key = cls.api_key
@@ -29,4 +37,4 @@ class PlayList():
             return duration
 
     def show_best_video(self):
-        pass
+        return self.like_count
