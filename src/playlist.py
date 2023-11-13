9bc8b987ff8b81e309412c8b8cc46a2920a4a404
belvvika <134885414+belvvika@users.noreply.github.com>
2023-11-13 12:30:49 +0300

    update playlist.py

--- src/playlist.py
+++ src/playlist.py
@@ -9,7 +9,7 @@ import isodate
 class PlayList(Channel, Video):
     load_dotenv()
     api_key = os.getenv('YOUTUBE_API')
-    def __init__(self, video_id, channel_id, playlist_id) -> None:
+    def __init__(self, playlist_id, video_id, channel_id) -> None:
         self.playlist_id = playlist_id
 
         super().__init__(video_id)
@@ -22,6 +22,8 @@ class PlayList(Channel, Video):
 
         self.title = playlist_videos['items'][0]
         self.url = f'https://www.youtube.com/watch?v=WNpfcqhNGhM&list={playlist_videos}'
+
+
     def get_service(cls):
         youtube_api_key = cls.api_key
         youtube = build('youtube', 'v3', developerKey=youtube_api_key)
@@ -31,14 +33,15 @@ class PlayList(Channel, Video):
     def total_duration(self):
         if self.video_statistic is not None and 'items' in self.video_statistic:
             duration = datetime.timedelta()
-            for video in video_statistic['items']:
+            for video in self.video_statistic['items']:
                 iso_8601_duration = video['contentDetails']['duration']
                 duration = isodate.parse_duration(iso_8601_duration)
         return duration
 
     def show_best_video(self):
+        playlist_videos = self.get_service().playlistItems().list(playlistId=playlist_id, part='contentDetails', maxResults=50).execute()
         video_ids: list[str] = [video['contentDetails']['videoId'] for video in playlist_videos['items']]
         for video_id in video_ids:
-            video_statistic = get_service().videos().list(part='snippet,statistics,contentDetails,topicDetails', id=video_id).execute()
+            video_statistic = self.get_service().videos().list(part='snippet,statistics,contentDetails,topicDetails', id=video_id).execute()
             like_count: int = video_statistic['items'][0]['statistics']['likeCount']
         return like_count
