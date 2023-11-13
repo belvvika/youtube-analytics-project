d9fe082c4ad32a004ef9629b4cfbc1e9de7c751b
belvvika <134885414+belvvika@users.noreply.github.com>
2023-11-13 12:44:04 +0300

    update playlist.py

--- src/playlist.py
+++ src/playlist.py
@@ -2,27 +2,25 @@ import isodate
 from googleapiclient.discovery import build
 import os
 from dotenv import load_dotenv
-from src.channel import Channel
-from src.video import Video
 import datetime
 import isodate
-class PlayList(Channel, Video):
+class PlayList:
     load_dotenv()
     api_key = os.getenv('YOUTUBE_API')
-    def __init__(self, playlist_id, video_id, channel_id) -> None:
+
+    def __init__(self, playlist_id):
         self.playlist_id = playlist_id
 
-        super().__init__(video_id)
-        super().__init__(channel_id)
+        self.playlist_info = self.get_service().playlists().list(id=playlist_id, part='contentDetails, snippet', ).execute()
 
-        playlist_videos = self.get_service().playlistItems().list(playlistId=playlist_id, part='contentDetails', maxResults=50).execute()
-        '''получение данных по плейлистам канала'''
+        self.playlist_videos = self.get_service().playlistItems().list(playlistId=playlist_id,part='contentDetails, snippet',maxResults=50).execute()
 
-        self.video_statistic = self.get_service().videos().list(part='snippet,statistics,contentDetails,topicDetails',id=video_id).execute()
+        self.title = self.playlist_info['items'][0]['snippet']['title']
+        self.url = f'https://www.youtube.com/playlist?list={self.playlist_id}'
 
-        self.title = playlist_videos['items'][0]
-        self.url = f'https://www.youtube.com/watch?v=WNpfcqhNGhM&list={playlist_videos}'
+        self.video_ids: list[str] = [video['contentDetails']['videoId'] for video in self.playlist_videos['items']]
 
+        self.video_statistic = self.get_service().videos().list(part='contentDetails,statistics',id=','.join(self.video_ids)).execute()
 
     def get_service(cls):
         youtube_api_key = cls.api_key
@@ -31,17 +29,26 @@ class PlayList(Channel, Video):
 
     @property
     def total_duration(self):
-        if self.video_statistic is not None and 'items' in self.video_statistic:
-            duration = datetime.timedelta()
+        '''
+        возвращает суммарную длительность плейлиста
+        '''
+            total_duration = datetime.timedelta()
             for video in self.video_statistic['items']:
                 iso_8601_duration = video['contentDetails']['duration']
                 duration = isodate.parse_duration(iso_8601_duration)
-        return duration
+                totla_duration += datetime.timedelta(seconds=duration.total_seconds())
+            return total_duration
 
     def show_best_video(self):
-        playlist_videos = self.get_service().playlistItems().list(playlistId=playlist_id, part='contentDetails', maxResults=50).execute()
-        video_ids: list[str] = [video['contentDetails']['videoId'] for video in playlist_videos['items']]
-        for video_id in video_ids:
-            video_statistic = self.get_service().videos().list(part='snippet,statistics,contentDetails,topicDetails', id=video_id).execute()
-            like_count: int = video_statistic['items'][0]['statistics']['likeCount']
-        return like_count
+        '''
+        возращает ссылку на самое популярное видео из плейлиста
+        '''
+        max_likes = 0
+        max_likes_video_id = ''
+        for item in self.video_statistic['items']:
+            video_id = item['id']
+            like_count = int(item['statistics']['likeCount'])
+            if like_count > max_likes:
+                max_likes = like_count
+                max_likes_video_id = video_id
+        return f'https://youtu.be/{max_likes_video_id}'
